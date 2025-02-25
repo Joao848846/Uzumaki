a11d7dbf0b6810d6854b5482e8da71cc78e453c6
João Victor <jvs.devsilva@gmail.com>
2025-02-24 21:46:39 -0300

    Add Variaveis class to demonstrate basic variable usage
    This commit introduces the Variaveis class, which declares and initializes several types of variables (int, double, String, char, boolean). It also includes code to print these variables to the console, serving as an example of variable declaration and output in Java.

---
+++ src/Variaveis.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int idade = 23;
+        double altura = 1.75;
+        String nome = "João";
+        char sexo = 'M';
+        boolean casado = false;
+        double peso = 88.5;
+        // Exibindo variáveis
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Casado: " + casado);
+    }
+}
