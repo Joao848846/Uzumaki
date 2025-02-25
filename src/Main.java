41e33567757e518da64642ce4a16f31d502b7742
João Victor <jvs.devsilva@gmail.com>
2025-02-25 20:41:25 -0300

    Add Calculadora de IMC and Main classes with age calculation logic
    Added a Calculadora IMC class to compute and display age based on the current year and birth year, along with a Main class to demonstrate its functionality. Includes proper instantiation and method invocation for testing.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-        // Instanciando a classe Calculadora
-        Calculadora calc = new Calculadora(2025, 2001, "João");
-        // Chamando o método calcularIdade
-        calc.calcularIdade();
+        Imc imc = new Imc("João", 80, 1.75);
+        imc.imctotal();
     }
 }
