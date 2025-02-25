7f91a2f12df788727e70d966d6b72d61360b2f8a
João Victor <jvs.devsilva@gmail.com>
2025-02-24 21:38:43 -0300

    Add new input fields and components to the main window
    Introduced a text field, checkbox, and password field to the frame with absolute positioning. Updated the layout to null and added corresponding labels for better UX.

--- Main.java
+++ Main.java
@@ -5,8 +5,22 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello World");
         frame.setSize(350, 450);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new JLabel("Hello World"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel labelnome = new JLabel("nome:");
+        labelnome.setBounds(30, 30, 50, 20);
+        JTextField textnome = new JTextField();
+        textnome.setBounds(70, 30, 200, 20);
+        JCheckBox check = new JCheckBox("check");
+        check.setBounds(50, 50, 100, 40);
+        JPasswordField password = new JPasswordField("123");
+        password.setBounds(50, 100, 80, 20);
+        frame.setLayout(null);
+        frame.getContentPane().add(labelnome);
+        frame.getContentPane().add(textnome);
+        frame.getContentPane().add(check);
+        frame.getContentPane().add(password);
         frame.setVisible(true);
 
     }
