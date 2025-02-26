faaec1e195cb5a0b1c2ad5a93e7e1f3784110aac
João Victor <jvs.devsilva@gmail.com>
2025-02-25 21:59:39 -0300

    Colocando em pratica o Scanner junto coma lógica de tratar os dados recebidos no sistema

---
+++ src/Nota.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class Nota {
+
+    public static void main(String[] args) {
+
+        Scanner notas = new Scanner(System.in);
+        System.out.println("Digite a primeira nota: ");
+         double nota1 = notas.nextInt();
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = notas.nextInt();
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = notas.nextInt();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média das notas é: " + media);
+        notas.close();
+    }
+}
