c6ccb43782598539058adbd6b517cd4bc385c44f
João Victor <jvs.devsilva@gmail.com>
2025-03-12 09:12:49 -0300

    Aplicando o conceito de como os tipos primitivos são ser tratados

---
+++ pratica/Primitivas.java
@@ -0,0 +1,19 @@
+package pratica;
+
+public class Primitivas {
+
+    public static void main(String[] args) {
+
+        int inteiro = 0;
+        double decimal = 0;
+        char caractere = 0;
+        boolean logico = false;
+
+        System.out.println(inteiro);
+        System.out.println(decimal);
+        System.out.println(caractere);
+        System.out.println(logico);
+
+
+    }
+}
